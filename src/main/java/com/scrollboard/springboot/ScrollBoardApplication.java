3d0150826aeebbd8b4e556d871f955474d57d720
KimJunKi <wnsrl7023@naver.com>
2020-01-05 16:27:00 +0900

    프로젝트 생성 및 의존성 추가.

---
+++ src/main/java/com/scrollboard/springboot/ScrollBoardApplication.java
@@ -0,0 +1,13 @@
+package com.scrollboard.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrollBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrollBoardApplication.class, args);
+	}
+
+}
