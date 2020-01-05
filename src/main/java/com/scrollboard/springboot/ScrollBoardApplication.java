42d9619da6b728d0ecf2608a1763539bd1f68e08
KimJunKi <wnsrl7023@naver.com>
2020-01-05 16:57:52 +0900

    Vue.js bulid path static 하위로 변경, database.properties 보안 외부화 설정 vue 연동

--- src/main/java/com/scrollboard/springboot/ScrollBoardApplication.java
+++ src/main/java/com/scrollboard/springboot/ScrollBoardApplication.java
@@ -1,13 +1,14 @@
 package com.scrollboard.springboot;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class ScrollBoardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ScrollBoardApplication.class, args);
+		//SpringApplication.run(ScrollBoardApplication.class, args);
+		new SpringApplicationBuilder(ScrollBoardApplication.class).properties("spring.config.location=" +"classpath:/db.properties").run(args);
 	}
 
 }
